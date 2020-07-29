e02885b15de22506f90230d6dae039d32d9af4db
Erick-SN <ericksn0707@gmail.com>
2020-07-28 23:15:46 -0500

    useFetch

---
+++ src/components/useFetch/CustomHooks.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import '../useEffect/effect.css';
+const CustomHooks = () => {
+  return (
+    <>
+      <h1> From Customhooks</h1>
+    </>
+  );
+};
+
+export default CustomHooks;
